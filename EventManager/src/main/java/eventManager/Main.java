3d6d94f23899ddf9657689da6ee24773bc030089
eabhakenny17 <eabhakenny17@gmail.com>
2025-11-04 23:06:36 +0000

    making sure everything is up to date

--- EventManager/src/main/java/eventManager/Main.java
+++ EventManager/src/main/java/eventManager/Main.java
@@ -6,13 +6,13 @@ public class Main
 	{	            
 //		UserAccount user = new UserAccount("Kelly", "1234", 1001);
 //
-//    Event e1 = new Event("Kelly's Baby Shower", "X207", 100, 45, 250.0, "Bring gifts!");
-//    e1.setUserId(user.getId());
-//    EventManager.addEvent(e1);
+//		Event e1 = new Event("Kelly's Baby Shower", "X207", 100, 45, 250.0, "Bring gifts!");
+//		e1.setUserId(user.getId());
+//    	EventManager.addEvent(e1);
 //
-//    Event e2 = new Event("Office Party", "V305", 50, 30, 500.0, "Formal attire required.");
-//    e2.setUserId(user.getId());
-//    EventManager.addEvent(e2);
+//    	Event e2 = new Event("Office Party", "V305", 50, 30, 500.0, "Formal attire required.");
+//    	e2.setUserId(user.getId());
+//    	EventManager.addEvent(e2);
     
 		new InitialScreenGUI();
 	}
