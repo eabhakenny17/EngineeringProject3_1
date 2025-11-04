b39041b6e35a2ab64b0d58bf54e72b794d6e2439
eabhakenny17 <eabhakenny17@gmail.com>
2025-11-04 22:30:23 +0000

    [CODE FROM JACK] Admin Login

---
+++ EventManager/src/main/java/eventManager/AdminLoginGUI.java
@@ -0,0 +1,91 @@
+package eventManager;
+
+import java.awt.FlowLayout;
+import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import eventManager.UserHomescreenGUI;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class AdminLoginGUI extends JFrame implements ActionListener
+{	
+	private AdminLogin adminlogin = new AdminLogin();
+	
+	private JButton loginButton = new JButton("Login");
+	
+	private JPanel panel = new JPanel();
+
+	private JLabel nameLabel = new JLabel("Name: ");
+	private JLabel passwordLabel = new JLabel("Password: ");
+	
+	private TextArea nameText = new TextArea();
+	private TextArea passwordText = new TextArea();
+	
+	private JLabel messageLabel = new JLabel("If Admin Account is invalid Contact: bossman@company.ie");
+	
+	JFrame window = new JFrame("Admin");
+	
+	FlowLayout flowLayout = new FlowLayout();
+	
+	private UserAccount loggedInUser; 
+	
+	public AdminLoginGUI() 
+	{
+		// Setup initial window
+		window.setLayout(flowLayout);
+				
+		loginButton.addActionListener(this);
+		panel.add(nameLabel);
+		panel.add(nameText);
+		panel.add(passwordLabel);
+		panel.add(passwordText);
+		panel.add(loginButton);
+			
+		panel.add(messageLabel);
+			
+			
+		window.getContentPane().add(panel);
+		window.pack();
+		window.setVisible(true);
+			
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		
+		if(e.getSource().equals(loginButton))
+		{
+            ArrayList<UserAccount> users = UserAccount.getUserList();
+            UserAccount loggedInUser = null;
+
+            for (UserAccount u : users) {
+                if (u.getName().equals(nameText.getText()) && u.getPassword().equals(passwordText.getText())) {
+                    loggedInUser = u;
+                    break;
+                }
+            }
+			int userId = adminlogin.attemptLogin(nameText.getText(), passwordText.getText());
+			
+			if (userId == 0)
+			{
+				messageLabel.setText("Error: Invalid username or password");
+				window.pack();
+			}
+			else
+			{
+//				new UserHomescreenGUI(login.signInViaID(userId));
+//				window.setVisible(false);
+				new UserHomescreenGUI(loggedInUser);
+	            window.dispose(); 
+			}
+		}
+		
+	}
+}
